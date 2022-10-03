9b642055907cbabaae4ea33b83d5e89c0d59ab11
alexisrojas1999 <alexisrojas09.ar@gmail.com>
2022-10-03 09:26:55 -0600

    fixed bug not allowing to select prices

---
+++ src/main/java/com/speedup/qa/interactions/Wait.java
@@ -0,0 +1,2 @@
+package com.speedup.qa.interactions;public class Wait {
+}
--- src/main/java/com/speedup/qa/task/Search.java
+++ src/main/java/com/speedup/qa/task/Search.java
@@ -1,5 +1,6 @@
 package com.speedup.qa.task;
 
+import com.speedup.qa.interactions.Wait;
 import com.speedup.qa.models.DataSearch;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
@@ -25,23 +26,21 @@ public class Search implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Click.on(LOCATION_CLICK));
-        actor.attemptsTo(Click.on(LOCATION.of(dataSearch.getLocation())));
-        actor.attemptsTo(Click.on(SELECT_ROOM));
-        actor.attemptsTo(Click.on(ROOM.of(dataSearch.getRoom())));
-        actor.attemptsTo(Click.on(SELECT_ADULT));
-        actor.attemptsTo(Click.on(ADULT.of(dataSearch.getAdult())));
-        actor.attemptsTo(Click.on(SELECT_CHILD));
-        actor.attemptsTo(Click.on(CHILDREN.of(dataSearch.getChild())));
-        actor.attemptsTo(Enter.theValue(dataSearch.getCheckout()).into(CHECKOUT));
-        actor.attemptsTo(Enter.theValue(dataSearch.getCheckin()).into(CHECKIN));
-        actor.attemptsTo(Click.on( SEARCH_BUTTON));
-        actor.attemptsTo(Click.on( SELECT_HOTEL));
+        actor.attemptsTo(Click.on(LOCATION_CLICK), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(LOCATION.of(dataSearch.getLocation())), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(SELECT_ROOM), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(ROOM.of(dataSearch.getRoom())), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(SELECT_ADULT), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(ADULT.of(dataSearch.getAdult())), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(SELECT_CHILD), Wait.waitFor(1));
+        actor.attemptsTo(Click.on(CHILDREN.of(dataSearch.getChild())) ,Wait.waitFor(1));
+        actor.attemptsTo(Enter.theValue(dataSearch.getCheckout()).into(CHECKOUT), Wait.waitFor(1));
+        actor.attemptsTo(Enter.theValue(dataSearch.getCheckin()).into(CHECKIN),Wait.waitFor(1));
+        actor.attemptsTo(Click.on( SEARCH_BUTTON), Wait.waitFor(1));
 
         WebElementFacade PrecioHotel1 = BrowseTheWeb.as(actor).findBy(com.speedup.qa.userinterfaces.Search.GET_PRICE_HOTEL1.getCssOrXPathSelector());
         WebElementFacade PrecioHotel2 = BrowseTheWeb.as(actor).findBy(com.speedup.qa.userinterfaces.Search.GET_PRICE_HOTEL2.getCssOrXPathSelector());
-        WebElementFacade PrecioHotel3 = BrowseTheWeb.as(actor).findBy(GET_PRICE_HOTEL3.getCssOrXPathSelector());
-
+        WebElementFacade PrecioHotel3 = BrowseTheWeb.as(actor).findBy(com.speedup.qa.userinterfaces.Search.GET_PRICE_HOTEL3.getCssOrXPathSelector());
 
 
 
@@ -56,11 +55,10 @@ public class Search implements Task {
         int Precio2=Integer.parseInt(String.valueOf(t2.deleteCharAt(0)));
         int Precio3=Integer.parseInt(String.valueOf(t3.deleteCharAt(0)));
 
-
-
-        System.out.println(Precio1);
-        System.out.println(Precio2);
-        System.out.println(Precio3);
+        System.out.println("el precio de los hoteles  son");
+        System.out.println("$"+Precio1);
+        System.out.println("$"+Precio2);
+        System.out.println("$"+Precio3);
 
 
         int menorvalor;
@@ -74,23 +72,25 @@ public class Search implements Task {
             actor.attemptsTo(Click.on(CLICK_BUTTON3));
             menorvalor = Precio3;
         }
+        System.out.println("El hotel menos costoso es de $"+menorvalor);
 
-        String checkinDate = DataSearch.getCheckin();
-        String checkoutDate = DataSearch.getCheckout();
+        String checkinDate = dataSearch.getCheckin();
+        String checkoutDate = dataSearch.getCheckout();
 
         LocalDate ci = LocalDate.parse(checkinDate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
         LocalDate co = LocalDate.parse(checkoutDate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
         long numdias = ci.until(co, ChronoUnit.DAYS)+1;
 
-        System.out.println(numdias);
+        System.out.println("reservacion para \n"+numdias +" dias");
 
 
-        //PRECIO TOTAL
         WebElementFacade PrecioTOTAL = BrowseTheWeb.as(actor).findBy(GET_PRICE_TOTAL.getCssOrXPathSelector());
 
         StringBuilder PT = new StringBuilder(PrecioTOTAL.getText());
         double PrecioT=Double.parseDouble(String.valueOf(PT.deleteCharAt(0)));
-        System.out.println(PrecioT);
+
+        System.out.println("el precio mas barrato es para" +numdias +" dias\n"+"$"+PrecioT);
+
 
         double preciototalcalculado = numdias*menorvalor;
 
